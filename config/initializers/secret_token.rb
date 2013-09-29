8b51613f7ecd31d7163fb3dd1c9a145acaa868f5
advishnuprasad <advishnuprasad@gmail.com>
2013-09-29 23:20:43 +0530

    Bootstrap Added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlbShare::Application.config.secret_key_base = '02839e826e524926939ec7686179e2f62412b7ec5c84c3a8fa98d569e9601d192f147aa873414a625fdd034c68dae2ead58aed5a47d65d515a23e64ea7125c4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SlbShare::Application.config.session_store :cookie_store, key: '_slb_share_session'
