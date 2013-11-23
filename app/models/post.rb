4daf146111ea7ab9c3be53512a3c3c643345dc23
advishnuprasad <advishnuprasad@gmail.com>
2013-11-24 02:47:32 +0530

    Associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :title, :description, :user
+  AvailableTags = %w(Rails Ruby RoR Js Jquery Gems)
   def user_name
     self.user.name
   end
