28b4271572e7d368ae46b274ed07db98a964d43c
advishnuprasad <advishnuprasad@gmail.com>
2014-09-08 23:45:01 +0530

    Validate Url in Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   acts_as_taggable
   belongs_to :user
-  validates_presence_of :title, :links, :user
+  validates_presence_of :title, :user
+  validates :links, :presence => true, :url => true
   AvailableTags = %w(Rails Ruby RoR Js Jquery Gems)
   def user_name
     self.user.name
